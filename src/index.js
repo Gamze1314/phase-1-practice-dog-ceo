a116c49651a571acd7585467bba97294bf5e91a5
gamze <gamzekandara@gmail.com>
2023-12-27 22:53:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,27 @@
 
- // Url = "https://dog.ceo/api/breeds/image/random/4";
-
-
 fetch("https://dog.ceo/api/breeds/image/random/4")
-  .then((resp) => {
-    if (!resp.ok) {
-      throw new Error(`Network response was not ok: ${resp.status}`);
-    }
-    return resp.json();
-  })
+  .then((resp) => resp.json())
   .then((data) => renderImg(data.message))
-  .catch((error) => console.error('Fetch error:', error));
+  // .catch((error) => console.error('Fetch error:', error))
 
   // data.message is an array containing urls of images
 
   function renderImg (urlArray) {
  // forEach  to get each url from the array
-
+// grab dog-image-container
    let divForImg = document.getElementById('dog-image-container')
     urlArray.forEach(url => {
-        let imgTag = document.createElement('img');
-        imgTag.src = url
-       
-        divForImg.append(imgTag)
+      // for each url create img tag
+        let imgTag = document.createElement('img')
+        imgTag.src = url // set src to url
+        divForImg.append(imgTag) // append it to DOM
     });
   }
-
-
-  fetch("https://dog.ceo/api/breeds/list/all")
-  .then((resp) => resp.json())
-  .then((data) => renderBreeds(Object.keys(data.message)))
-  .then((data) => filterBreed(Object.keys(data.message)))
-
 
   function renderBreeds(breedsArray) { 
     let ul = document.getElementById('dog-breeds')
     breedsArray.forEach(breed => {
-        let li = document.createElement('li');
+        let li = document.createElement('li')
         li.textContent = breed
         ul.appendChild(li)
     })
@@ -44,24 +29,24 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
 
 //   // if the user selects 'a' in the dropdown, only show the breeds with names that start with the letter a
 
+fetch("https://dog.ceo/api/breeds/list/all") // fetch dogs
+.then((resp) => resp.json())
+.then((data) => filterBreed(data)) // call filter function 
+
 function filterBreed(data) {
-    let breedsArray = Object.keys(data.message);
+    let breedsArray = Object.keys(data.message) //
+    console.log(breedsArray)
+
   
     function handleChange(e) {
-      let filtered = breedsArray.filter((breed) => breed.startsWith(e.target.value));
-      let ul = document.getElementById('dog-breeds');
-      ul.textContent = "";
+
+      let filtered = breedsArray.filter((breed) => breed.startsWith(e.target.value))
+      let ul = document.getElementById('dog-breeds')
+      ul.textContent = ""
       
-      renderBreeds(filtered);
+      renderBreeds(filtered)
     }
   
-    let breedDropdown = document.querySelector('#breed-dropdown');
-    breedDropdown.addEventListener('change', handleChange);
-  
-  
-    handleChange({ target: { value: breedDropdown.value } });
-  }
-  
-  fetch("https://dog.ceo/api/breeds/list/all")
-    .then((resp) => resp.json())
-    .then((data) => filterBreed(data));
+    let breedDropdown = document.querySelector('#breed-dropdown')
+    breedDropdown.addEventListener('change', handleChange)
+  } 
